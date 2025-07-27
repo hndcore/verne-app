6903bb55fdf241bdd905a5afa01224bc9c6c9112
Manuel L. Camarena <w.mlc32@gmail.com>
2025-07-27 13:26:09 +0200

    chore: dependencies and base features

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,52 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { globalIgnores } from "eslint/config";
+import jsx from "eslint-plugin-react";
+import prettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  [
+    globalIgnores(["dist", "build"]),
+    {
+      files: ["**/*.{js,jsx,ts,tsx}"],
+      languageOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        parser: tseslint.parser,
+        parserOptions: {
+          ecmaFeatures: {
+            jsx: true,
+          },
+        },
+        globals: {
+          ...globals.browser,
+          React: "writable",
+        },
+      },
+      plugins: {
+        react: jsx,
+        "react-hooks": reactHooks,
+        "react-refresh": reactRefresh,
+      },
+      rules: {
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+      },
     },
-  },
-])
+    {
+      files: ["**/*.test.{ts,tsx}"],
+      languageOptions: {
+        globals: {
+          ...globals.jest,
+        },
+      },
+    },
+    prettier,
+  ],
+  storybook.configs["flat/recommended"],
+);
