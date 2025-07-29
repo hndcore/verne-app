af468775ee2719e3f16c829864f680f96b5c1f2f
Manuel L. Camarena <w.mlc32@gmail.com>
2025-07-29 23:56:48 +0200

    fix(book-table): fix query client provider error on tests

--- src/test/pages/home.test.tsx
+++ src/test/pages/home.test.tsx
@@ -1,10 +1,15 @@
 import Home from "@/pages/home";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render, screen } from "@testing-library/react";
 import { describe, test, expect } from "vitest";
 
 describe("Home", () => {
   test("renders properly", () => {
-    render(<Home />);
+    render(
+      <QueryClientProvider client={new QueryClient()}>
+        <Home />
+      </QueryClientProvider>,
+    );
     expect(screen.getByTestId("home-page")).toBeInTheDocument();
     expect(screen.getByTestId("home-main")).toBeInTheDocument();
   });
